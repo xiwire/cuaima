40d3fcb89d618c884b3ca19dab306849218215c5
jxpp <jxpp@chigui.re>
2021-01-24 14:11:18 +0100

    implement graceful stop of scheduling process

--- cuaima/scheduler.py
+++ cuaima/scheduler.py
@@ -1,9 +1,27 @@
+import atexit
 import sched
 from multiprocessing import Pipe, Process
 
 from time import time, sleep
 
 from cuaima.synth import InstantiableSynth
+
+
+STOP_MESSAGE = 'STOP'
+
+
+def polling(pipe, scheduler):
+    while 1:
+        if pipe.poll(0):
+            received = pipe.recv()
+            # I have to do it this way since Synth instances can't be
+            # pickled. Maybe in a future rewrite of Synth this can change
+            if received == STOP_MESSAGE:
+                break
+            synth_name = received.pop('synth')
+            synth_instance = InstantiableSynth.get_by_name(synth_name)
+            scheduler.enter(action=synth_instance.play, **received)
+        scheduler.run(blocking=False)
 
 
 def rescheduling_action(f, delay, scheduler, delay_func=None, *args, **kwargs):
@@ -23,31 +41,21 @@ class Scheduler:
     def __init__(self):
         self._scheduler = sched.scheduler(time, sleep)
         self.conn, self.child_conn = Pipe()
+        self.process = Process(target=polling, args=(self.child_conn, self._scheduler))
+        atexit.register(self._stop)
         self._launch()
 
     def schedule(self, synth: InstantiableSynth, **kwargs):
         """ Schedule an action
         """
-        # schedulable_action = rescheduling_action(target.play, 1, self._scheduler, **kwargs)
-        # self._scheduler.enter(delay=1, priority=0, action=schedulable_action)
-
-        self.conn.send(dict(delay=1, priority=0, synth=synth.name))
+        self.conn.send(dict(delay=1, priority=0, synth=synth.name, kwargs=kwargs))
 
     def _launch(self):
-        p = Process(target=self._polling, args=(self.child_conn, self._scheduler))
-        p.start()
+        self.process.start()
 
-    def _polling(self, pipe, scheduler):
-        while 1:
-            if pipe.poll(0):
-                received = pipe.recv()
-                # I have to do it this way since Synth instances can't be
-                # pickled. Maybe in a future rewrite of Synth this can change
-                synth_name = received.pop('synth')
-                print(synth_name)
-                synth_instance = InstantiableSynth.get_by_name(synth_name)
-                scheduler.enter(action=synth_instance.play, **received)
-            scheduler.run(blocking=False)
+    def _stop(self):
+        self.conn.send(STOP_MESSAGE)
+        self.process.join()
 
 
 DEFAULT_SCHEDULER = Scheduler()
